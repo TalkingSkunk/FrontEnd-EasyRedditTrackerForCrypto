e4128064e0efd3652c7ebe85e4faf51620af502e
Richard Yang <richard.yang.tong@gmail.com>
2021-02-02 15:59:08 -0500

    Reddit API demo with search function (#31)
    * Added a dropdown menu for quantity limit on search items.
    
    * Added a search button to the search bar.
    
    * Attached an event listener to submit button.
    
    * Added a new function for submit button.
    
    * Added a error message to display when input is empty.
    
    * Added a feature so search will capture the value set by limit dropdown menu.
    
    * Converted the error message into bootstrap style.
    
    * Commented out the function that is not working.
    
    * Added a new feature that clears the input form when search button is clicked.
    
    * Created a module to store the function that fetches the reddit API.
    
    * Added a fetch function that returns reddit data by term, limit, and sort by.
    
    * Further digging in the reddit api to get the data array.
    
    * Updating the fetch function so it now returns the reddit data we want.
    
    * Added a catch to the fetch call.
    
    * Added the fetch function to submit event listener.
    
    * Added a features that the reddit search results will be printed in a series of cards.
    
    * Updated the search function so it now adds image to card.
    
    * Updated the search function so it now adds url and text to the cards.

---
+++ reddit-api.js
@@ -0,0 +1,9 @@
+const searchReddit = (searchTerm, searchLimit, sortBy) => {
+    return fetch(`https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`)
+    .then(response => response.json())
+    .then(data => data.data.children.map(data => data.data))
+    .catch(err => console.log(err));
+};
+
+// Test
+searchReddit("gamestop", 25, "latest");
