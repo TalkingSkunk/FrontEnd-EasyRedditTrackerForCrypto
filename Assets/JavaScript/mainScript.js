733b7f041675a46344c629a6e08e1aa34b1a0262
Richard Yang <richard.yang.tong@gmail.com>
2021-02-05 15:04:56 -0500

    Added a new feature to store data from crypto API calls to local storage.

--- Assets/JavaScript/mainScript.js
+++ Assets/JavaScript/mainScript.js
@@ -1,12 +1,17 @@
 // Feature: Initializing a new data base.
 // Starts here
 var cryptoDB = {};
-var cryptoList = ['bitcoin', 'ethereum', 'tether'];
+var cryptoList = ['bitcoin', 'ethereum', 'tether', 'polkadot', 'cardano', 'litecoin', 'chainlink'];
     
 for (let i = 0; i < cryptoList.length; i++) {
     cryptoDB[cryptoList[i]] = {
+        id: "",
+        symbol: "",
         name: "",
-        currentPrice: 0,
+        image: "",
+        priceChange: "",
+        percentChange: "",
+        lastUpdated: ""
     }
 }
 
@@ -22,3 +27,23 @@ const emptyReddits = (coin) => {
 
 cryptoList.forEach(coin => emptyReddits(coin));
 // Ends here
+
+// Crpto API call
+// Start here
+async function getDataCrypto(crypto) {
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&ids=${crypto}`;
+    const [result] = await fetch(url).then((res) => res.json());
+    
+    let cryptoObj = cryptoDB[crypto];
+    cryptoObj.id = result.id;
+    cryptoObj.symbol = result.symbol;
+    cryptoObj.name = result.name;
+    cryptoObj.image = result.image;
+    cryptoObj.priceChange= result.price_change_24h.toFixed(2);
+    cryptoObj.percentChange = result.price_change_percentage_24h.toFixed(2);
+    cryptoObj.lastUpdated = result.last_updated;
+};
+
+cryptoList.forEach(coin => getDataCrypto(coin));
+// End here
+
